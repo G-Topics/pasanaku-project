f1e71530aa1b4878080bafeca068677dbbb2bc33
m_acuna <marcoantonioraiacuna@gmail.com>
2024-03-15 20:57:30 -0400

    Creating models...

---
+++ app/Models/Moneda.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Moneda extends Model
+{
+    protected $table = 'moneda';
+    public $timestamps = false;
+    protected $fillable = [
+        'id', 'nombre', 'smonetario',
+    ];
+}
