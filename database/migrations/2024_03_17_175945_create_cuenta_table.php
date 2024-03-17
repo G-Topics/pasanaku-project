dec323e3c195cc8bfbb104d43dc9f5c6e6b65e5c
unknown <facundocuellar@gmail.com>
2024-03-17 15:53:53 -0400

    migraciones

---
+++ database/migrations/2024_03_17_175945_create_cuenta_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cuenta', function (Blueprint $table) {
+            $table->integer('id')->unsigned()->primary();
+            $table->string('nrocuenta');
+            $table->foreignId('IDmoneda')->constrained('moneda');
+            $table->foreignId('CIjugador')->constrained('jugador');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cuenta');
+    }
+};
